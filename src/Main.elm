3b258aff740147d4ba947ef09ecc381ff27a65f3
Ennio Mara <enniomara@users.noreply.github.com>
2022-01-19 20:42:34 +0100

    Initial commit example code showing shortify entries

---
+++ src/Main.elm
@@ -0,0 +1,89 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, li, text, ul)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+type alias Item =
+    { name : String }
+
+
+type alias Items =
+    List Item
+
+
+init : Items
+init =
+    [ { name = "hn" }
+    , { name = "pomodoro" }
+    , { name = "shortify-gh" }
+    ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Items -> Items
+update msg _ =
+    case msg of
+        Increment ->
+            [ { name = "hej" }, { name = "ho" } ]
+
+        Decrement ->
+            [ { name = "nej" }, { name = "nejjj" } ]
+
+
+listFirstItem : Maybe Item -> String
+listFirstItem mItem =
+    case mItem of
+        Just item ->
+            item.name
+
+        Nothing ->
+            "nothing"
+
+
+
+-- VIEW
+
+
+view : Items -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , ul []
+            (List.map renderItem model)
+
+        -- [ text (listFirstItem <| List.head model)
+        -- ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+renderItem : Item -> Html Msg
+renderItem item =
+    li []
+        [ text item.name ]
