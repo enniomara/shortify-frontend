a94fc18965cf4cc2130122a0f5c838aeb01f05b9
Ennio Mara <enniomara@users.noreply.github.com>
2022-01-19 23:13:54 +0100

    Add test for filter function

---
+++ tests/MainTest.elm
@@ -0,0 +1,21 @@
+module MainTest exposing (..)
+
+import Expect
+import Main exposing (Item, Items)
+import Test exposing (..)
+
+
+filterItems : Test
+filterItems =
+    test "right amount of items when needle is found"
+        (\_ ->
+            let
+                items =
+                    [ Item "hn", Item "shortify", Item "pomodoro" ]
+            in
+            items
+                -- Matches shortify and pomodoro
+                |> Main.filter (Just "or")
+                |> List.length
+                |> Expect.equal 2
+        )
