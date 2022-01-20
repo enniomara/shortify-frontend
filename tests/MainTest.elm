26f9c9348c050e7d816f0d6598f0a2de964a36c1
Ennio Mara <enniomara@users.noreply.github.com>
2022-01-20 18:14:51 +0100

    Wrap test in describe

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -7,15 +7,17 @@ import Test exposing (..)
 
 filterItems : Test
 filterItems =
-    test "right amount of items when needle is found"
-        (\_ ->
-            let
-                items =
-                    [ Item "hn", Item "shortify", Item "pomodoro" ]
-            in
-            items
-                -- Matches shortify and pomodoro
-                |> Main.filter (Just "or")
-                |> List.length
-                |> Expect.equal 2
-        )
+    describe "Filtering functionality"
+        [ test "right amount of items when needle is found"
+            (\_ ->
+                let
+                    items =
+                        [ Item "hn", Item "shortify", Item "pomodoro" ]
+                in
+                items
+                    -- Matches shortify and pomodoro
+                    |> Main.filter (Just "or")
+                    |> List.length
+                    |> Expect.equal 2
+            )
+        ]
