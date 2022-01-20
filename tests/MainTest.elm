d987ace217914ae6b56074335f49fcf69738c365
Ennio Mara <enniomara@users.noreply.github.com>
2022-01-20 18:16:38 +0100

    Wrap filter tests in filter container

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -8,28 +8,30 @@ import Test exposing (..)
 filterItems : Test
 filterItems =
     describe "Filtering functionality"
-        [ test "right amount of items when needle is found"
-            (\_ ->
-                let
-                    items =
-                        [ Item "hn", Item "shortify", Item "pomodoro" ]
-                in
-                items
-                    -- Matches shortify and pomodoro
-                    |> Main.filter (Just "or")
-                    |> List.length
-                    |> Expect.equal 2
-            )
-        , test "same item when needle is empty"
-            (\_ ->
-                let
-                    items =
-                        [ Item "hn", Item "shortify", Item "pomodoro" ]
-                in
-                items
-                    -- Matches shortify and pomodoro
-                    |> Main.filter Nothing
-                    |> List.length
-                    |> Expect.equal 3
-            )
+        [ describe "filter tests"
+            [ test "right amount of items when needle is found"
+                (\_ ->
+                    let
+                        items =
+                            [ Item "hn", Item "shortify", Item "pomodoro" ]
+                    in
+                    items
+                        -- Matches shortify and pomodoro
+                        |> Main.filter (Just "or")
+                        |> List.length
+                        |> Expect.equal 2
+                )
+            , test "same item when needle is empty"
+                (\_ ->
+                    let
+                        items =
+                            [ Item "hn", Item "shortify", Item "pomodoro" ]
+                    in
+                    items
+                        -- Matches shortify and pomodoro
+                        |> Main.filter Nothing
+                        |> List.length
+                        |> Expect.equal 3
+                )
+            ]
         ]
